47b7f0bbedd3ccab662c1302dbf81c1d3587a7e3
duckie <sarthakmisraa@gmail.com>
2021-03-01 21:11:34 +0530

    added the mothership util

---
+++ motherShip/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type malware struct {
+	Name    string
+	Md5sum  string
+	Sha1sum string
+}
+
+var samples = make(map[string]malware)
+
+func addSample(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	if _, ok := samples[r.PostForm["sha256sum"][0]]; ok {
+		w.Write([]byte("Alreay have this sample.\n"))
+		w.Write([]byte("Thank you for your time"))
+		return
+	}
+
+	samples[r.PostForm["sha256sum"][0]] = malware{
+		Name:    r.PostForm["name"][0],
+		Md5sum:  r.PostForm["md5sum"][0],
+		Sha1sum: r.PostForm["sha1sum"][0],
+	}
+
+	fmt.Println(samples)
+	w.Write([]byte("Sample Added.\n"))
+	w.Write([]byte("Thank you"))
+
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	temp, _ := template.ParseFiles("malwareSamples.html")
+	temp.Execute(w, samples)
+}
+
+func main() {
+
+	http.HandleFunc("/addSample", addSample)
+	http.HandleFunc("/", home)
+	err := http.ListenAndServe(":8080", nil)
+	log.Println("Starting listening on Port 8080...")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
